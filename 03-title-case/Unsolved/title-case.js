34b83730524b3c2eaf279ecade7ce97febd37103
LilTzar <eeoerkeedu@gmail.com>
2022-06-15 11:27:54 -0600

    solved title case with split, toUpperCase, slice, and join

--- 03-title-case/Unsolved/title-case.js
+++ 03-title-case/Unsolved/title-case.js
@@ -1,3 +1,18 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function (str) {
+	const arr = str.split(" ");
+	console.log(arr);
+	let capitalized = "";
+
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+		capitalized = arr.join(" ");
+		console.log(capitalized);
+	}
+
+	return capitalized;
+};
+
+titleCase();
