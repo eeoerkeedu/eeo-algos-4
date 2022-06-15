812d8a4348982da41d2d15af0d34d4b38f2ed7b3
LilTzar <eeoerkeedu@gmail.com>
2022-06-15 09:27:35 -0600

    solved palendrome with split, reverse, and join

--- 01-is-palindrome/Unsolved/is-palindrome.js
+++ 01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,10 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function (str) {};
+var isPalindrome = function (str) {
+	const reverse = [...str].reverse().join("");
+	if (str === reverse) {
+		return true;
+	} else {
+		return false;
+	}
+};
