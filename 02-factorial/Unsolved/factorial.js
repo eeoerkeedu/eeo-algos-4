2b95b91731b21fb970a91e032a5e1f295b6a855b
LilTzar <eeoerkeedu@gmail.com>
2022-06-15 10:43:59 -0600

    solved factorial, had to properly define variables

--- 02-factorial/Unsolved/factorial.js
+++ 02-factorial/Unsolved/factorial.js
@@ -1,3 +1,19 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function (num) {
+	let fac = 1;
+
+	if (num <= 1) {
+		return 1;
+	} else {
+		for (let i = 1; i <= num; i++) {
+			console.log(i, num);
+			fac = fac * i;
+		}
+		console.log(fac);
+	}
+
+	return fac;
+};
+
+factorial();
